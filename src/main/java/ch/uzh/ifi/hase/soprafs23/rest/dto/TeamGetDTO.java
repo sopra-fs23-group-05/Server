58eb4bf3dae564e96146e693e83f7fc1feaeb556
tom7689 <tomkaspar.meier@uzh.ch>
2023-04-04 21:08:43 +0200

    less dublicate

--- src/main/java/ch/uzh/ifi/hase/soprafs23/rest/dto/TeamGetDTO.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs23/rest/dto/TeamGetDTO.java
@@ -19,51 +19,27 @@ public class TeamGetDTO {
 
     private int idxClueGiver;
 
-    public void setIdxClueGiver(int idxClueGiver) {
-        this.idxClueGiver = idxClueGiver;
-    }
+    public void setIdxClueGiver(int idxClueGiver) {this.idxClueGiver = idxClueGiver;}
 
-    public int getIdxClueGiver() {
-        return idxClueGiver;
-    }
+    public int getIdxClueGiver() {return idxClueGiver;}
 
-    public List<Player> getPlayers() {
-        return players;
-    }
+    public List<Player> getPlayers() {return players;}
 
-    public void setPlayers(List<Player> players) {
-        this.players = players;
-    }
+    public void setPlayers(List<Player> players) {this.players = players;}
 
-    public void setaRole(Role aRole) {
-        this.aRole = aRole;
-    }
+    public void setaRole(Role aRole) {this.aRole = aRole;}
 
-    public Role getaRole() {
-        return aRole;
-    }
+    public Role getaRole() {return aRole;}
 
-    public void setTeamId(int teamId) {
-        this.teamId = teamId;
-    }
+    public void setTeamId(int teamId) {this.teamId = teamId;}
 
-    public int getTeamId() {
-        return teamId;
-    }
+    public int getTeamId() {return teamId;}
 
-    public void setPoints(int points) {
-        this.points = points;
-    }
+    public void setPoints(int points) {this.points = points;}
 
-    public int getPoints() {
-        return points;
-    }
+    public int getPoints() {return points;}
 
-    public String getTeamName() {
-        return teamName;
-    }
+    public String getTeamName() {return teamName;}
 
-    public void setTeamName(String teamName) {
-        this.teamName = teamName;
-    }
+    public void setTeamName(String teamName) {this.teamName = teamName;}
 }
