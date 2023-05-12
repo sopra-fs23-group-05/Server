de8102981bcb39e0e258cf9ee5ad2bc36f9374f3
felixmerz <65616000+felixmerz00@users.noreply.github.com>
2023-05-12 11:38:25 +0200

    pages ws sessions #215

--- src/main/java/ch/uzh/ifi/hase/soprafs23/websockets/PageWebSocketHandler.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs23/websockets/PageWebSocketHandler.java
@@ -1,33 +1,62 @@
 package ch.uzh.ifi.hase.soprafs23.websockets;
 
+import ch.uzh.ifi.hase.soprafs23.service.GameService;
+import org.springframework.web.server.ResponseStatusException;
 import org.springframework.web.socket.CloseStatus;
 import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
 import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
 
 public class PageWebSocketHandler extends TextWebSocketHandler {
 
-    private final List<WebSocketSession> webSocketSessions = new ArrayList<>();
+    /** HashMap that stores a list of sessions for each access code. */
+    private final HashMap<Integer, ArrayList<WebSocketSession>> webSocketSessions = new HashMap<>();
+
+    // Inject dependency to GameService here
+    private final GameService gameService;
+
+    public PageWebSocketHandler(GameService gameService) {
+        this.gameService = gameService;
+    }
 
     @Override
     public void afterConnectionEstablished(WebSocketSession session) {
-        webSocketSessions.add(session);
+        int accessCode = getAccessCode(session);
+        // If the access code is not in the HashMap, add it
+        if (!webSocketSessions.containsKey(accessCode)) {
+            webSocketSessions.put(accessCode, new ArrayList<>());
+        }
+        webSocketSessions.get(accessCode).add(session);
     }
 
     @Override
     protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+        int accessCode = getAccessCode(session);
         System.out.println("Sending message: " + message.getPayload());
 
-        for (WebSocketSession webSocketSession : webSocketSessions) {
+        for (WebSocketSession webSocketSession : webSocketSessions.get(accessCode)) {
             webSocketSession.sendMessage(message);
         }
     }
 
     @Override
     public void afterConnectionClosed(WebSocketSession session, CloseStatus status) {
-        webSocketSessions.remove(session);
+        int accessCode = getAccessCode(session);
+        webSocketSessions.get(accessCode).remove(session);
+
+        // If the game was deleted, delete the mapping.
+        try{
+            gameService.getGame(accessCode);
+        }catch (ResponseStatusException e){
+            webSocketSessions.remove(accessCode);
+        }
+    }
+
+    /** Extracts the access code from a WebSocketSession object. */
+    private static int getAccessCode(WebSocketSession session) {
+        return Integer.parseInt(session.getUri().toString().substring(session.getUri().toString().lastIndexOf('/') + 1));
     }
 }
