cb52df994a6477646ba1ffa465a46ac3526624da
tom7689 <tomkaspar.meier@uzh.ch>
2023-05-16 17:35:25 +0200

    new try

---
+++ src/main/java/ch/uzh/ifi/hase/soprafs23/custom/Timer.java
@@ -0,0 +1,34 @@
+package ch.uzh.ifi.hase.soprafs23.custom;
+
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+
+import java.io.IOException;
+import java.util.List;
+
+public class Timer extends Thread {
+    private final List<WebSocketSession> webSocketSessions;
+    private int timerValue;
+
+    public Timer(List<WebSocketSession> webSocketSessions, int timerValue) {
+        this.webSocketSessions = webSocketSessions;
+        this.timerValue = timerValue;
+    }
+
+    @Override
+    public void run() {
+        try {
+            for (int tick = timerValue; tick >= 0; tick--) {
+                String time = String.valueOf(timerValue);
+                for (WebSocketSession webSocketSession : webSocketSessions) {
+                    webSocketSession.sendMessage(new TextMessage(time));
+                }
+                System.out.println("Sending message: " + time);
+                timerValue--;
+                Thread.sleep(1000);
+            }
+        } catch (InterruptedException | IOException e) {
+            // Handle any exceptions that occur during timer execution
+        }
+    }
+}
