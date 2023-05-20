60acc78f80e845f023c0fc87ff7da103a8d40400
felixmerz <65616000+felixmerz00@users.noreply.github.com>
2023-05-20 11:42:23 +0200

    chat gpt try, not working

--- src/test/java/ch/uzh/ifi/hase/soprafs23/websockets/WebSocketConfigTest.java
+++ src/test/java/ch/uzh/ifi/hase/soprafs23/websockets/WebSocketConfigTest.java
@@ -1,40 +1,49 @@
 package ch.uzh.ifi.hase.soprafs23.websockets;
 
+import ch.uzh.ifi.hase.soprafs23.service.GameService;
+import ch.uzh.ifi.hase.soprafs23.service.LobbyService;
+import ch.uzh.ifi.hase.soprafs23.service.UserService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-import javax.websocket.ContainerProvider;
-import javax.websocket.DeploymentException;
-import javax.websocket.WebSocketContainer;
-
-import java.io.IOException;
-import java.net.URI;
-
-import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 // Source: https://stackoverflow.com/questions/37899268/test-websocket-in-spring-boot-application
 
-@ExtendWith(SpringExtension.class)
 @SpringBootTest
-@WebAppConfiguration
+@WebMvcTest(WebSocketConfig.class)
 class WebSocketConfigTest {
 
-    private WebSocketContainer container;
-    private TestWebSocketClient client;
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Mock
+    GameService gameService;
+
+    @Mock
+    private LobbyService lobbyService;
+
+    @Mock
+    private UserService userService;
+
 
     @BeforeEach
     void setUp() {
-        container = ContainerProvider.getWebSocketContainer();
-        client = new TestWebSocketClient();
     }
 
     @Test
-    void createSession() throws DeploymentException, IOException {
-        container.connectToServer(client, URI.create("ws://localhost:8080/timers/123456"));
-        assertNotNull(client.session);
+    void testWebSocketConnection() throws Exception {
+        // Send a GET request to the TIMER_ENDPOINT
+        ResultActions resultActions = mockMvc.perform(MockMvcRequestBuilders.get("/timers/123456"));
+
+        // Assert that the request was successful (HTTP 200 OK)
+        resultActions.andExpect(status().isOk());
     }
 }
