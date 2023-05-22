25d2d3f741bfb11f3cdfb374b8c710fff2f242be
tom7689 <tomkaspar.meier@uzh.ch>
2023-05-22 11:10:41 +0200

    Message Tests

---
+++ src/test/java/ch/uzh/ifi/hase/soprafs23/websockets/MessageTest.java
@@ -0,0 +1,30 @@
+package ch.uzh.ifi.hase.soprafs23.websockets;
+
+import ch.uzh.ifi.hase.soprafs23.constant.MessageType;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MessageTest {
+
+    @Test
+    public void testGettersAndSetters() {
+
+        Message message = new Message(123456, 1, "Test message", MessageType.DESCRIPTION);
+
+        assertEquals(123456, message.getAccessCode());
+        assertEquals(1, message.getSenderId());
+        assertEquals("Test message", message.getContent());
+        assertEquals(MessageType.DESCRIPTION, message.getType());
+
+        message.setAccessCode(123455);
+        message.setSenderId(2L);
+        message.setContent("test");
+        message.setType(MessageType.GUESS);
+
+        assertEquals(123455, message.getAccessCode());
+        assertEquals(2, message.getSenderId());
+        assertEquals("test", message.getContent());
+        assertEquals(MessageType.GUESS, message.getType());
+    }
+}
